3b6e7a9085f061d39f799d110f1401703e115ada
HariKrishnaAllu4545 <harikrishna@opstronomy.com>
2024-05-07 07:32:02 +0530

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SignupModule } from './signup/signup.module';
+import { LoginModule } from './login/login.module';
+import { JwtModule } from '@nestjs/jwt';
+import { OrderController } from './order/order.controller';
+// import { Order } from './order/order';
+import { OrderService } from './order/order.service';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { OrderModule } from './order/order.module';
+import { StockinfoModule } from './stockinfo/stockinfo.module';
+import * as crypto from 'crypto';
+const mongoDBUrl = process.env.MONGO;
+
+@Module({
+  imports: [MongooseModule.forRoot(mongoDBUrl), SignupModule, LoginModule
+    , JwtModule.registerAsync({
+      useFactory: () => ({
+          secret: crypto.randomBytes(32).toString('hex'),
+          signOptions: { expiresIn:'30d'},
+        }),
+    }), OrderModule, StockinfoModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  
+}
